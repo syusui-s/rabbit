88d8ebdaffe0f9e263081844b2e0cf02337404d3
syusui-s <syusui-s@users.noreply.github.com>
2023-12-16 01:50:16 +0000

    deploy: a0eaf90563a71155a69f123d93c6f7647ba3ecff

--- assets/NotFound-eff72fcb.js
+++
@@ -1,2 +0,0 @@
-import{t}from"./index-78d6b60c.js";const e=t('<div class="container mx-auto max-w-[640px] py-10"><h1 class="text-4xl font-bold text-stone-700">お探しのページは見つかりませんでした</h1><p class="pt-4"><a class="text-blue-500 underline" href="/">← トップに戻る'),a=()=>e();export{a as default};
-//# sourceMappingURL=NotFound-eff72fcb.js.map
