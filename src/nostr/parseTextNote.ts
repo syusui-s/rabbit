533f67ea98b99283de0f8ef353d441e42e7748d1
Shusui MOYATANI <syusui.s@gmail.com>
2023-12-22 02:11:25 +0900

    fix: type

--- src/nostr/parseTextNote.ts
+++ src/nostr/parseTextNote.ts
@@ -1,10 +1,11 @@
 import { nip19 } from 'nostr-tools';
-import { DecodeResult } from 'nostr-tools/lib/nip19';
 
 import isValidId from '@/nostr/event/isValidId';
 import TagsBase from '@/nostr/event/TagsBase';
 
 const { decode } = nip19;
+
+type DecodeResult = nip19.DecodeResult;
 
 export type PlainText = {
   type: 'PlainText';
