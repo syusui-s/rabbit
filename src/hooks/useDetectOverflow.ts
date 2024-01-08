0be6d195397a6be68fd396f77165e9ea34e368fb
Shusui MOYATANI <syusui.s@gmail.com>
2024-01-08 09:44:47 +0900

    fix: detect overflow

--- src/hooks/useDetectOverflow.ts
+++ src/hooks/useDetectOverflow.ts
@@ -41,7 +41,7 @@ const useDetectOverflow = () => {
           });
         },
         {
-          threshold: 0,
+          threshold: [0, 0.25, 0.5, 0.75, 1],
         },
       );
       observer.observe(elementRef);
