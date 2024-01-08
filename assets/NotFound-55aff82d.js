f59255657fe19fa037a9b6e0b36b5eb632adc4a6
syusui-s <syusui-s@users.noreply.github.com>
2024-01-08 08:04:19 +0000

    deploy: 38ea2123874c8e81934db8b036269834cdaebe5c

---
+++ assets/NotFound-55aff82d.js
@@ -0,0 +1,2 @@
+import{t}from"./index-2136e1f5.js";const a=t('<div class="container mx-auto max-w-[640px] py-10"><h1 class="text-4xl font-bold text-fg">お探しのページは見つかりませんでした</h1><p class=pt-4><a class="text-link underline"href=/>← トップに戻る'),e=()=>a();export{e as default};
+//# sourceMappingURL=NotFound-55aff82d.js.map
