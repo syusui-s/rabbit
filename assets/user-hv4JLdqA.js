87275f1929c48a167d84f36bc86846f0a9a85fa9
syusui-s <syusui-s@users.noreply.github.com>
2024-01-21 15:28:48 +0000

    deploy: 371636e2ad480416ac072994cbd0efd9be0df0f0

--- assets/user-hv4JLdqA.js
+++
@@ -1,2 +0,0 @@
-import{s as e,t as o}from"./index-yaV19nBq.js";const s=o('<svg xmlns=http://www.w3.org/2000/svg fill=none stroke=currentColor stroke-width=1.5 aria-hidden=true data-slot=icon viewBox="0 0 24 24"><path stroke-linecap=round stroke-linejoin=round d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0M4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632">'),a=(r={})=>(()=>{const t=s();return e(t,r,!0,!0),t})();export{a as U};
-//# sourceMappingURL=user-hv4JLdqA.js.map
