53cb07170f20b5522037b9f974ac19c557bb8adc
syusui-s <syusui-s@users.noreply.github.com>
2024-01-25 03:50:29 +0000

    deploy: a13fbdd07a78a85415214fcddabdc04d1b640047

---
+++ assets/NotFound-bCzuUlTs.js
@@ -0,0 +1,2 @@
+import{u as a,i as s,t as i}from"./index-WwbqkKbH.js";const r=i('<div class="container mx-auto max-w-[640px] py-10"><h1 class="text-4xl font-bold text-fg"></h1><p class=pt-4><a class="text-link underline"href=/>'),u=()=>{const t=a();return(()=>{const n=r(),e=n.firstChild,l=e.nextSibling,o=l.firstChild;return s(e,()=>t.t("notFound.title")),s(o,()=>t.t("notFound.back")),n})()};export{u as default};
+//# sourceMappingURL=NotFound-bCzuUlTs.js.map
---
+++ assets/arrow-left-TNe2Ato_.js
@@ -0,0 +1,2 @@
+import{s as o,t as e}from"./index-WwbqkKbH.js";const s=e('<svg xmlns=http://www.w3.org/2000/svg fill=none stroke=currentColor stroke-width=1.5 aria-hidden=true data-slot=icon viewBox="0 0 24 24"><path stroke-linecap=round stroke-linejoin=round d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18">'),a=(r={})=>(()=>{const t=s();return o(t,r,!0,!0),t})();export{a as A};
+//# sourceMappingURL=arrow-left-TNe2Ato_.js.map
