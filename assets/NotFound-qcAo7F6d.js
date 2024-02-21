973871733e31978f6f3056f213e8d9d8a66dee0f
syusui-s <syusui-s@users.noreply.github.com>
2024-02-21 18:04:38 +0000

    deploy: 4769a16845b9ef29dc2f792702129c7a500a82fe

--- assets/NotFound-qcAo7F6d.js
+++
@@ -1,2 +0,0 @@
-import{u as a,i as s,t as i}from"./index-D14T-05y.js";const r=i('<div class="container mx-auto max-w-[640px] py-10"><h1 class="text-4xl font-bold text-fg"></h1><p class=pt-4><a class="text-link underline"href=/>'),u=()=>{const t=a();return(()=>{const n=r(),e=n.firstChild,l=e.nextSibling,o=l.firstChild;return s(e,()=>t.t("notFound.title")),s(o,()=>t.t("notFound.back")),n})()};export{u as default};
-//# sourceMappingURL=NotFound-qcAo7F6d.js.map
