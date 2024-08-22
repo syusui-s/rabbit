8376b44b8a3a484937c56a00b69ec284ad305ef3
Shusui MOYATANI <syusui.s@gmail.com>
2024-08-23 01:22:22 +0900

    feat: use nostr-wasm

--- src/nostr/usePool.ts
+++ src/nostr/usePool.ts
@@ -1,17 +1,24 @@
 import { createRoot, createSignal } from 'solid-js';
 
+import { AbstractSimplePool } from 'nostr-tools/abstract-pool';
 import { AbstractRelay } from 'nostr-tools/abstract-relay';
-import { SimplePool } from 'nostr-tools/pool';
 import { normalizeURL } from 'nostr-tools/utils';
+import { setNostrWasm, verifyEvent } from 'nostr-tools/wasm';
+import { initNostrWasm } from 'nostr-wasm';
 
-class Pool extends SimplePool {
+
+class Pool extends AbstractSimplePool {
   getRelay(relayUrl: string): AbstractRelay | undefined {
     return this.relays.get(normalizeURL(relayUrl));
   }
 }
 
+initNostrWasm()
+  .then(setNostrWasm)
+  .catch((err) => console.error(err));
+
 const createPool = () => {
-  const newPool = new Pool();
+  const newPool = new Pool({ verifyEvent });
   newPool.trackRelays = true;
   return newPool;
 };
