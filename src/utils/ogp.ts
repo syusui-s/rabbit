d410328de38c941cbb30814803f6b2fe878d5034
Shusui MOYATANI <syusui.s@gmail.com>
2024-11-08 00:41:34 +0900

    fix: encoding of fetched OGP

--- src/utils/ogp.ts
+++ src/utils/ogp.ts
@@ -9,7 +9,41 @@ export type OgpContent = {
   image: string;
 };
 
-export const parseOgpFromDOM = (doc: HTMLDocument, urlString: string): OgpContent | null => {
+export const getCharset = (source: ArrayBuffer): string | null => {
+  const content = new TextDecoder('US-ASCII').decode(source.slice(0, 1024));
+
+  const metaContentTypeMatch = content.match(
+    /<meta\s+http-equiv="Content-Type"\s+content="text\/html;\s+charset=([-A-Za-z0-9_]+)"/i,
+  );
+  if (metaContentTypeMatch != null) {
+    return metaContentTypeMatch[1].toLowerCase();
+  }
+
+  const metaCharsetMatch = content.match(/<meta\s+charset="([-A-Za-z0-9]+)"/i);
+  if (metaCharsetMatch != null) {
+    return metaCharsetMatch[1].toLowerCase();
+  }
+
+  return null;
+};
+
+// https://developer.mozilla.org/en-US/docs/Web/API/TextDecoder/decode
+const supportedEncodings = new Set(['utf-8', 'shift_jis', 'shift-jis', 'euc-jp', 'iso-2022-jp']);
+
+export const fixEncoding = (source: ArrayBuffer): string => {
+  try {
+    const charset = getCharset(source) ?? 'utf-8';
+    if (!supportedEncodings.has(charset)) {
+      throw new Error(`Unsupported encodings: ${charset}`);
+    }
+    const decoder = new TextDecoder(charset);
+    return decoder.decode(source);
+  } catch (e) {
+    throw new Error('Failed to convert charset', { cause: e });
+  }
+};
+
+export const parseOgpFromDOM = (doc: Document, urlString: string): OgpContent | null => {
   const props: { [property: string]: string } = {};
 
   Array.from(doc.head.querySelectorAll('meta')).forEach((m) => {
@@ -20,18 +54,15 @@ export const parseOgpFromDOM = (doc: HTMLDocument, urlString: string): OgpConten
     }
   });
 
+  // Required
   const image = props['og:image'] || props['twitter:image'];
   const title = props['og:title'] || props['twitter:title'];
-  const description = props['og:description'] || props['twitter:description'] || '';
   const url = props['og:url'] || urlString;
+  // Optional
+  const description = props['og:description'] || props['twitter:description'] || '';
 
-  if (image != null && title != null && description != null && url) {
-    return {
-      title,
-      description,
-      image,
-      url,
-    } satisfies OgpContent;
+  if (image && title && url) {
+    return { title, description, image, url } satisfies OgpContent;
   }
   return null;
 };
@@ -57,17 +88,23 @@ export const buildUrl = (urlString: string): string => {
 };
 
 export const fetchOgpContent = async (urlString: string): Promise<OgpContent | null> => {
-  if (!isOgpUrl(urlString)) return null;
+  try {
+    if (!isOgpUrl(urlString)) return null;
 
-  const url = buildUrl(urlString);
+    const url = buildUrl(urlString);
 
-  const res = await fetch(url, {
-    mode: 'cors',
-    headers: { Accept: 'text/html' },
-    credentials: 'omit',
-  });
-  const text = await res.text();
-  return parseOgp(text, urlString);
+    const res = await fetch(url, {
+      mode: 'cors',
+      headers: { Accept: 'text/html' },
+      credentials: 'omit',
+    });
+    const arrayBuffer = await res.arrayBuffer();
+    const encodedContent = fixEncoding(arrayBuffer);
+    return parseOgp(encodedContent, urlString);
+  } catch (err) {
+    console.error(`failed to fetch ogp for ${urlString}`, err);
+    return null;
+  }
 };
 
 export type UseOgpProps = {
