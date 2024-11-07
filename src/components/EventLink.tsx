4253d9ce98c12feed669bbc9168a6aab6838ff1e
Shusui MOYATANI <syusui.s@gmail.com>
2024-11-08 01:01:31 +0900

    fix: EventLink show NIP-21 formatted URL

--- src/components/EventLink.tsx
+++ src/components/EventLink.tsx
@@ -1,4 +1,4 @@
-import { Show, type Component } from 'solid-js';
+import { createMemo, Show, type Component } from 'solid-js';
 
 import { noteEncode, neventEncode } from 'nostr-tools/nip19';
 
@@ -16,21 +16,33 @@ const tryEncodeNote = (eventId: string) => {
   }
 };
 
-const tryEncodeNevent = (eventId: string) => {
+const tryEncodeNevent = (eventId: string, kind: number) => {
   try {
-    return neventEncode({ id: eventId });
+    return neventEncode({ id: eventId, kind });
   } catch (err) {
     console.error('failed to encode event id into Bech32 entity (NIP-19) but ignore', eventId, err);
     return eventId;
   }
 };
 
-const EventLink: Component<EventLinkProps> = (props) => (
-  <button class="text-link underline">
-    <Show when={props.kind == null || props.kind === 1} fallback={tryEncodeNevent(props.eventId)}>
-      {tryEncodeNote(props.eventId)}
+const EventLink: Component<EventLinkProps> = (props) => {
+  const nip19 = createMemo(() =>
+    props.kind == null || props.kind === 1
+      ? tryEncodeNote(props.eventId)
+      : tryEncodeNevent(props.eventId, props.kind),
+  );
+
+  const url = () => (nip19() !== props.eventId ? `nostr:${nip19()}` : null);
+
+  return (
+    <Show when={url()} fallback={<span>{props.eventId}</span>} keyed>
+      {(href) => (
+        <a class="text-link underline" href={href}>
+          {nip19()}
+        </a>
+      )}
     </Show>
-  </button>
-);
+  );
+};
 
 export default EventLink;
