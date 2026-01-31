3a049b4310b5de6f18cb263c207efca8649a9058
syusui-s <syusui-s@users.noreply.github.com>
2026-01-31 04:08:51 +0000

    deploy: 320977a1bcbb11caf67c146da423b7ec48f2ed51

---
+++ assets/NotFound-iMwyb3_a.js
@@ -0,0 +1,2 @@
+import{u as a,t as i,i as s}from"./index-9cbshM_8.js";const r=i('<div class="container mx-auto max-w-screen-sm py-10"><h1 class="text-4xl font-bold text-fg"></h1><p class=pt-4><a class="text-link underline"href=/>'),u=()=>{const t=a();return(()=>{const n=r(),e=n.firstChild,l=e.nextSibling,o=l.firstChild;return s(e,()=>t.t("notFound.title")),s(o,()=>t.t("notFound.back")),n})()};export{u as default};
+//# sourceMappingURL=NotFound-iMwyb3_a.js.map
